4c9a4841164899049bea6783977bc8de5a8f046d
ali Sajadi <chargoonmobileintern2@outlook.com>
2020-09-24 08:39:46 +0330

    Create food category table view and its custom cell

---
+++ iRestaurantFinder/Controllers/Cells/FoodCategoryCell.swift
@@ -0,0 +1,24 @@
+//
+//  FoodCategoryCell.swift
+//  iRestaurantFinder
+//
+//  Created by Ali Sajadi on 9/24/20.
+//  Copyright © 2020 Ali Sajadi. All rights reserved.
+//
+
+import UIKit
+
+class FoodCategoryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
